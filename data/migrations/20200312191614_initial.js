ceccbd05cd0d80f300c4e5aac51f8edf9548042d
Jesse-Tingle <jtingle0@gmail.com>
2020-03-14 00:27:16 -0400

    ran zoos migration

--- data/migrations/20200312191614_initial.js
+++ data/migrations/20200312191614_initial.js
@@ -7,8 +7,27 @@ exports.up = async function(knex) {
 			.notNull()
 			.unique();
 	});
+
+	await knex.schema.createTable("animals", table => {
+		table.increments("id");
+		table.text("name").notNull();
+		table
+			.integer("species_id")
+			.references("id")
+			.inTable("species");
+	});
+
+	await knex.schema.createTable("species", table => {
+		table.increments("id");
+		table
+			.text("name")
+			.notNull()
+			.unique();
+	});
 };
 
 exports.down = async function(knex) {
 	await knex.schema.dropTableIfExists("zoos");
+	await knex.schema.dropTableIfExists("animals");
+	await knex.schema.dropTableIfExists("species");
 };
